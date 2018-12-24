277e357a1e4d149233b175d4f4d8293e3b0f6cb8
gmanroney <gerard.x.moroney@gmail.com>
2018-12-24 18:42:42 +0000

    Changes closed at this point (not in this commit) include #9 #8 #6 #10 #17 #4

--- commonFunctions.py
+++ commonFunctions.py
@@ -8,9 +8,13 @@ from pathlib import Path
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import csv
 import pdb
+import pdfkit
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
+from email import encoders
+from prettytable import PrettyTable
 
 # Helper function to dump files to csv file
 def dumpToCsv (mycol,mycolu,period,my_out_file):
@@ -139,22 +143,43 @@ Subject: %s
     except:  
        print ('Something went wrong...')
 
-#Helper function to send HTML Email (currenty assumes 4 column format with 4th column the sentiment value)
-def SendEmailByGmail (g_user, g_pass, e_user, e_subject, e_body ):
 
-    gmail_user = g_user
-    gmail_password = g_pass
-    sent_from = gmail_user
-    to=e_user
-    subject = e_subject
+# Helper function to generate plain text report simple format with no analytics
+def GenTextRptSimple (e_body):
+    record_count = 0
+    myPrettyTable = PrettyTable (["User","Date","Message","Score"])
+    myPrettyTable.align["User"]="l"
+    myPrettyTable.align["Message"]="l"
+    myPrettyTable.align["Score"]="l"
 
-    # Create message container
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = e_subject
-    msg['From'] = gmail_user
-    msg['To'] = e_user
+    with open(e_body, encoding="utf8") as csvFile:
+        reader = csv.DictReader(csvFile, delimiter=',')
+        record_count = record_count + 1 
+        for row in reader:
+            index = 0
+            rowout = ""
+            for fn in reader.fieldnames:
+                if index == 0:
+                    rowout = '"' +  row[fn] + '"'
+                    f0 = row[fn]
+                else:
+                    rowout = rowout + ',"' + row[fn] + '"'
+                    if index == 1:
+                        f1 = row[fn]
+                    elif index == 2:
+                        f2 = row[fn]
+                    else:
+                        f3 = row[fn]
+                index = index + 1
+            myPrettyTable.add_row([RemoveInvalidAscii(f0[:50]),f1,f2[:100],f3])
+            record_count = record_count + 1
+    if record_count > 0: 
+        print (myPrettyTable)
+    else:
+        print ("No records found")
 
-
+# Helper function to generate HTML report simple format with no analytics
+def GenHTMLRptSimple (e_body):
     # Create table that will display results in e-mail body
     table = ''
     with open(e_body, encoding="utf8") as csvFile:
@@ -214,8 +239,58 @@ Gerard
   </body>
 </html>
 """ % ( table )
+    return email_html
 
+#Helper function to send HTML Email (currenty assumes 4 column format with 4th column the sentiment value)
+def SendEmailByGmail (g_user, g_pass, e_user, e_subject, e_body ):
+
+    gmail_user = g_user
+    gmail_password = g_pass
+    sent_from = gmail_user
+    to=e_user
+    subject = e_subject
+
+    # Create message container
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = e_subject
+    msg['From'] = gmail_user
+    msg['To'] = e_user
+
+    email_html = GenHTMLRptSimple (e_body)
+    GenTextRptSimple (e_body)
+
+    out_html = '/tmp/out.html'
+    out_pdf = '/tmp/out.pdf'
+    if os.path.exists(out_html):
+        os.remove(out_html)
+    if os.path.exists(out_pdf):
+        os.remove(out_pdf)
+
+    with open(out_html, 'w') as writeFile:
+        writeFile.writelines(email_html)
+
+    # Generate body of e-mail from html
     msg.attach(MIMEText(email_html, 'html'))
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(out_html, "rb").read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="out.html"')
+    msg.attach(part)
+
+    # Make PDF file from html file and attach to e-mail
+    options = {
+            'page-size': 'A4',
+            'margin-top': '0.75in',
+            'margin-right': '0.75in',
+            'margin-bottom': '0.75in',
+            'margin-left': '0.75in',
+            }
+    pdfkit.from_file(out_html, out_pdf, options=options)
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open(out_pdf, "rb").read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="out.pdf"')
+    msg.attach(part)
 
     try:  
        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
